2321f84e8653335d4bfe0d6d8676a593fd285ca2
Jad Kik <jadkik94@gmail.com>
2021-02-28 14:37:50 +0100

    Disable UnselectAuto for ios mouse interaction issues
    It hijacks all clicks on the page and renders it unresponsive

--- packages/interaction/src/main.ts
+++ packages/interaction/src/main.ts
@@ -3,14 +3,14 @@ import { DateClicking } from './interactions/DateClicking'
 import { DateSelecting } from './interactions/DateSelecting'
 import { EventDragging } from './interactions/EventDragging'
 import { EventResizing } from './interactions/EventResizing'
-import { UnselectAuto } from './interactions/UnselectAuto'
+// import { UnselectAuto } from './interactions/UnselectAuto'
 import { FeaturefulElementDragging } from './dnd/FeaturefulElementDragging'
 import { OPTION_REFINERS, LISTENER_REFINERS } from './options'
 import './options-declare'
 
 export default createPlugin({
   componentInteractions: [DateClicking, DateSelecting, EventDragging, EventResizing],
-  calendarInteractions: [UnselectAuto],
+  // calendarInteractions: [UnselectAuto],
   elementDraggingImpl: FeaturefulElementDragging,
   optionRefiners: OPTION_REFINERS,
   listenerRefiners: LISTENER_REFINERS,
